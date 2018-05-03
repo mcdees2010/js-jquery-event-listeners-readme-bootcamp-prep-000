3be705003e7927775d97578a6cf48c57d0342133
Maria Dees <kingsgrl2010@yahoo.com>
2018-05-03 00:35:02 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -23,7 +23,7 @@ function pressIt() {
 function submitIt() {
   $("form").on("submit", function() {
     if ($( "input:first" ).val() {
-      alert()
+      alert("Your form is going to be submitted now.");
   }
 
 $(document).ready(function(){
