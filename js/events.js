e05a8df5c2553c06d6e90a7693a8df8d5a4e48c2
Maria Dees <kingsgrl2010@yahoo.com>
2018-05-03 00:35:18 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -22,7 +22,7 @@ function pressIt() {
 
 function submitIt() {
   $("form").on("submit", function() {
-    if ($( "input:first" ).val() {
+    if ($( "input:first" ).val()) {
       alert("Your form is going to be submitted now.");
   }
 
