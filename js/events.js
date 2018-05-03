fadf2e956837f1582a622b1d2b074292a3b4a5c4
Maria Dees <kingsgrl2010@yahoo.com>
2018-05-03 00:26:29 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -15,7 +15,7 @@ function frameIt() {
 function pressIt() {
   $(document).on('keydown', function(key) {
   if(key.which == 71){
-      alert('g was pressed');
+      alert('G was pressed');
   }
 });
 }
